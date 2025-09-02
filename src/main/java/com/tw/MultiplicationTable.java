e52d9be27fd2776aded16d7e80a90fb6e0c846d2
Canmor Lam <canmor.lam@gmail.com>
2025-09-02 13:13:31 +0800

    Update MultiplicationTable.java

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -5,27 +5,27 @@ public class MultiplicationTable {
         return null;
     }
 
-    public Boolean isValid(int start, int end) {
+    private Boolean isValid(int start, int end) {
         return null;
     }
 
-    public Boolean isInRange(int number) {
+    private Boolean isInRange(int number) {
         return null;
     }
 
-    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+    private Boolean isStartNotBiggerThanEnd(int start, int end) {
         return null;
     }
 
-    public String generateTable(int start, int end) {
+    private String generateTable(int start, int end) {
         return null;
     }
 
-    public String generateLine(int start, int row) {
+    private String generateLine(int start, int row) {
         return null;
     }
 
-    public String generateExpression(int multiplicand, int multiplier) {
+    private String generateExpression(int multiplicand, int multiplier) {
         return null;
     }
 }
